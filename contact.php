920df96b2692c37f6dacda4f33c0bc3c66ea227a
adityamaulanazidqy <adityamaullana234@gmail.com>
2024-06-29 12:57:17 +0700

    Informasi contact us diperbarui & pop up sudah diperbagus dengan sweetalert

--- contact.php
+++ contact.php
@@ -2,17 +2,13 @@
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
-
-  <title>Zstore Gaming sortware Shop</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <title>Zstore Gaming Software Shop</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
 
   <!-- Additional CSS Files -->
   <link rel="stylesheet" href="assets/css/fontawesome.css">
@@ -20,11 +16,61 @@
   <link rel="stylesheet" href="assets/css/owl.css">
   <link rel="stylesheet" href="assets/css/animate.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-  <!--
 
+  <!-- Sweetalert2 -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
+  <?php
+  // Menangani pengiriman formulir
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include_once 'php/koneksi.php';
+
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Menyiapkan pernyataan SQL
+    $sql = "INSERT INTO messages (name, surname, email, subject, message) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssss", $name, $surname, $email, $subject, $message);
+
+    // Menjalankan pernyataan SQL dan memeriksa apakah berhasil
+    if ($stmt->execute()) {
+      echo "<script>
+        Swal.fire({
+          title: 'Berhasil!',
+          text: 'Pesan berhasil dikirim!',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = 'contact.php';
+          }
+        });
+      </script>";
+    } else {
+      echo "<script>
+        Swal.fire({
+          title: 'Kesalahan!',
+          text: 'Terjadi kesalahan: " . $stmt->error . "',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = 'contact.php';
+          }
+        });
+      </script>";
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
+  ?>
 
   <!-- ***** Preloader Start ***** -->
   <div id="js-preloader" class="js-preloader">
@@ -46,16 +92,16 @@
         <div class="col-12">
           <nav class="main-nav">
             <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
+            <a href="index.php" class="logo">
               <img src="assets/images/logo.png" alt="" style="width: 158px;">
             </a>
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
             <ul class="nav">
-              <li><a href="index.html">Home</a></li>
-              <li><a href="shop.html">Our Shop</a></li>
-              <li><a href="product-details.html">Product Details</a></li>
-              <li><a href="contact.html" class="active">Contact Us</a></li>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="shop.php">Our Shop</a></li>
+              <li><a href="product-details.php">Product Details</a></li>
+              <li><a href="contact.php" class="active">Contact Us</a></li>
               <li><a href="#">Sign In</a></li>
             </ul>
             <a class='menu-trigger'>
@@ -89,9 +135,9 @@
               <h6>Contact Us</h6>
               <h2>Say Hello!</h2>
             </div>
-            <p>LUGX Gaming Template is based on the latest Bootstrap 5 CSS framework. This template is provided by
-              TemplateMo and it is suitable for your gaming shop ecommerce websites. Feel free to use this for any
-              purpose. Thank you.</p>
+            <p>ZSTORE adalah platform terpercaya untuk mendapatkan software game terbaik untuk laptop Anda. Di sini,
+              Anda akan menemukan berbagai pilihan game yang berkualitas tinggi dan terbaru, yang siap meningkatkan
+              pengalaman bermain Anda.</p>
             <ul>
               <li><span>Address</span> Sunny Isles Beach, FL 33160, United States</li>
               <li><span>Phone</span> 089531853860</li>
@@ -104,35 +150,30 @@
             <div class="row">
               <div class="col-lg-12">
                 <div id="map">
-                  <iframe
-                    src="https://www.google.com/maps/place/Jl.+Raya+Sumberejo,+Klitik,+Sumberejo,+Kec.+Wonoayu,+Kabupaten+Sidoarjo,+Jawa+Timur+61261/@-7.4404346,112.6604913,15.91z/data=!4m6!3m5!1s0x2dd7e18f94078625:0x1a751a041e733260!8m2!3d-7.4414617!4d112.6545033!16s%2Fg%2F11c645mphp?entry=ttu"
-                    width="100%" height="325px" frameborder="0" style="border:0; border-radius: 23px;"
-                    allowfullscreen=""></iframe>
+                  <iframe src="https://www.google.com/maps/place/Jl.+Raya+Sumberejo,+Klitik,+Sumberejo,+Kec.+Wonoayu,+Kabupaten+Sidoarjo,+Jawa+Timur+61261/@-7.4404346,112.6604913,15.91z/data=!4m6!3m5!1s0x2dd7e18f94078625:0x1a751a041e733260!8m2!3d-7.4414617!4d112.6545033!16s%2Fg%2F11c645mphp?entry=ttu" width="100%" height="325px" frameborder="0" style="border:0; border-radius: 23px;" allowfullscreen=""></iframe>
                 </div>
               </div>
               <div class="col-lg-12">
-                <form id="contact-form" action="./php/process_form.php" method="post">
+                <form id="contact-form" action="" method="post">
                   <div class="row">
                     <div class="col-lg-6">
                       <fieldset>
-                        <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
+                        <input type="text" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
                       </fieldset>
                     </div>
                     <div class="col-lg-6">
                       <fieldset>
-                        <input type="surname" name="surname" id="surname" placeholder="Your Surname..."
-                          autocomplete="on" required>
+                        <input type="text" name="surname" id="surname" placeholder="Your Surname..." autocomplete="on" required>
                       </fieldset>
                     </div>
                     <div class="col-lg-6">
                       <fieldset>
-                        <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..."
-                          required="">
+                        <input type="email" name="email" id="email" placeholder="Your E-mail..." required>
                       </fieldset>
                     </div>
                     <div class="col-lg-6">
                       <fieldset>
-                        <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on">
+                        <input type="text" name="subject" id="subject" placeholder="Subject..." autocomplete="on">
                       </fieldset>
                     </div>
                     <div class="col-lg-12">
@@ -173,6 +214,6 @@
   <script src="assets/js/counter.js"></script>
   <script src="assets/js/custom.js"></script>
 
-  </body>
+</body>
 
 </html>
